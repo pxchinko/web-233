482e5ae07840b0149da126b7ce81adefda47b1e6
Jonathan Roland <jdroland31@yahoo.com>
2020-05-31 16:18:04 -0400

    Initial commit Assignment 1.4: Primitive Types

---
+++ week-1/roland-assignment-1-4.js
@@ -0,0 +1,29 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Jonathan Roland
+; Date: 31 May 2020
+; Modified By: Your Name Here
+; Description: Practice with setting Primitive Types.
+;===========================================
+*/
+
+//set variables
+var firstName = "Jonathan";
+var lastName = "Roland";
+var age = 33;
+var street = "123 Fake Street";
+var city = "Gotham";
+var zipCode = 12345;
+
+//output variable values to console
+
+console.log("First Name: ",firstName);
+console.log("Last Name: ",lastName);
+console.log("Age: ",age);
+console.log("Street: ",street);
+console.log("City: ",city);
+console.log("Zip Code: ",zipCode);
+
+//alternate inline/concatenated output can be un-commented below and run. I tried to use '/n' newlines for presentation but they were parsed as string values on output in VS Code.
+// console.log("First Name: "+firstName+", Last Name: "+lastName+", Age: "+age+", Street: "+street+", City: "+city+", Zip Code: "+zipCode);
