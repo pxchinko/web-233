8d4f3aed452f3264eed1bde9ea58da162d197e29
Jonathan Roland <jdroland31@yahoo.com>
2020-06-07 19:39:32 -0400

    Added copy of Prof Krasso's web-231 repository, project header file, and week 2 folder and header test file.

---
+++ week-2/roland-header-test.js
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title:  roland-header-test.js
+; Author: Professor Krasso 
+; Date:   07 June 2020
+; Modified By: Jonathan Roland
+; Description: test of header file import from project root
+;===========================================
+*/
+
+//import the header file
+const header = require('../roland-header.js');
+//display the header file
+console.log(header.display('Jonathan','Roland','Exercise 2.2 - Header'));
