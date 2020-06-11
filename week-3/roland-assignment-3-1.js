b1b3a54edb6f547a422e9a4402c3f60e3b6d6924
Jonathan Roland <jdroland31@yahoo.com>
2020-06-11 17:11:08 -0400

    Assignment 3-1 was missing content, pushed content to file

---
+++ week-3/roland-assignment-3-1.js
@@ -0,0 +1,32 @@
+/*
+============================================
+; Title:  roland-assignment-3-1.js
+; Author: Jonathan Roland 
+; Date:   11 June 2020
+; Modified By: Jonathan Roland
+; Description: A switch statement with at least two errors.
+;===========================================
+*/
+
+//set a value to test against
+const test = "X";
+//a switch statement to test for certain value matches
+switch test{
+    case "A":
+        console.log("test is A");
+
+    case "E":
+        console.log("test is E");
+
+    case "H"
+        console.log("test is H");
+
+    case "P":
+        console.log("test is P");
+
+    case "T":
+        console.log("test is T");
+
+    case "X":
+        console.log("test is X");
+}
