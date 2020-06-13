28826707c62fb8ae0b4fc49ed158a6c4074763a7
Sarah Massie <smassie@my365.bellevue.edu>
2020-06-12 20:44:04 -0700

    Update roland-assignment-3-1.js

--- week-3/roland-assignment-3-1.js
+++ week-3/roland-assignment-3-1.js
@@ -1,24 +1,24 @@
 /*
 ============================================
 ; Title:  roland-assignment-3-1.js
-; Author: Jonathan Roland 
-; Date:   11 June 2020
-; Modified By: Jonathan Roland
+; Author: Jonathan Roland
+; Date:   12 June 2020
+; Modified By: Sarah Massie
 ; Description: A switch statement with at least two errors.
 ;===========================================
 */
 
-//set a value to test against
+// set a value to test against
 const test = "X";
-//a switch statement to test for certain value matches
-switch test{
+// a switch statement to test for certain value matches
+switch (test) { // () around test to put the constant into the function as a parameter
     case "A":
         console.log("test is A");
 
     case "E":
         console.log("test is E");
 
-    case "H"
+    case "H": // : after "H" to encompass what the case is
         console.log("test is H");
 
     case "P":
