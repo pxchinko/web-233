766a56802e0d64f3459f9f938a2e95839cb758f3
Jonathan Roland <jdroland31@yahoo.com>
2020-06-18 23:59:09 -0400

    Added assignment 4.1

---
+++ week-4/roland-assignment-4-1.js
@@ -0,0 +1,16 @@
+/*
+============================================
+; Title:  roland-assignment-4-1.js
+; Author: Jonathan Roland 
+; Date:   18 June 2020
+; Modified By: 
+; Description: A program utilizing an array with at least two errors.
+;===========================================
+*/
+
+var testArray === ("this","is","a","test","array");
+var output = "";
+for(i=1; i<testArray.length; i+){
+    output+=testArray[i]+" ";
+}
+console.log(output);
